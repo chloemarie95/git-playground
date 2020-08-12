82879a745882dc4d0db602abbd1cfb3b7c3366cf
Chloe McDermott <chloemcdermott95@aol.com>
2020-08-12 09:44:19 +0100

    Edited text file and commited folders

---
+++ Exercises/Fizzbuzz/fizzbuzz.py
@@ -0,0 +1,14 @@
+def fizzbuzz(number):
+    if (number % 3 == 0) and (number % 5 == 0):
+        print('FizzBuzz')
+    elif number % 5 == 0:
+        print('Buzz')
+    elif number % 3 == 0:
+        print('Fizz')
+    else:
+        pass
+
+
+fizzbuzz(15)
+
+# complete task on page 41
